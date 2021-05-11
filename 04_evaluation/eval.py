030653565ec007aa3392a4ec638c262fde5741f6
Jean-Baptiste Camps <jbcamps@hotmail.com>
2021-05-11 17:51:29 +0200

    eval script

---
+++ 04_evaluation/eval.py
@@ -0,0 +1,37 @@
+import fastwer
+import glob
+import pandas
+
+def compute_score(gt, folder):
+    """
+    :param gt: address of the file containing gt
+    :param folder: folder containing hypotheses to test
+    :return:
+    """
+    with open(gt, 'r') as f:
+        gt = f.readlines()
+
+    # gt2 = gt
+    hypotheses = {}
+    for eval in glob.glob(evals):
+        with open(eval, 'r') as f:
+            hypotheses[eval] = f.readlines()
+
+    CERs = []
+    WERs = []
+    keys = hypotheses.keys()
+
+    for key in keys:
+        CERs.append(fastwer.score(hypotheses[key], gt, char_level=True))
+        WERs.append(fastwer.score(hypotheses[key], gt))
+
+    d = {'CER': CERs, 'WER': WERs}
+
+    return pandas.DataFrame(data = d, index=keys)
+
+
+if __name__ == "__main__":
+    gt = "gt_fol11_nopunct.txt"
+    evals = "to_eval/*"
+
+    compute_score(gt, evals)
